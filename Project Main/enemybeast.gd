693699b422393579fec0cffe00bbf0813de52902
Shakakaka <shakaboom1234@gmail.com>
2024-07-09 17:40:54 -0400

    Add files via upload

--- Project Main/enemybeast.gd
+++ Project Main/enemybeast.gd
@@ -3,24 +3,38 @@ extends CharacterBody2D
 var speed= 70
 var player_chase = false
 var player = null
-@onready var animations = $AnimatedSprite2D
+@onready var animations = $AnimationPlayer
 var player_angle = 0
 var direction = "down"
+var isDead = false
+var knockbackPower = 1000
+var health = 100.0
+var maxHealth = health
+var stunned = false
+@onready var healthBar = $healthBar
+
+func set_health_bar():
+	healthBar.value = health
+	if health == maxHealth:
+		healthBar.visible = false
+	else: 
+		healthBar.visible = true
 
 func updateAnimation():
 	if velocity.is_zero_approx():
-		animations.stop()
+		animations.play("idle_"+direction)
 	else:
 		player_angle = rad_to_deg(get_angle_to(player.global_position))
-		if player_angle >= -150 and player_angle <= -30:
+		if player_angle >= -135 and player_angle <= -45:
 			direction = "up"
-		elif player_angle <= 150 and player_angle >= 30:
+		elif player_angle <= 135 and player_angle >= 45:
 			direction = "down"
-		elif player_angle >= 150 and player_angle <=210:
-			direction = "left"
-		elif player_angle >= -30 and player_angle <= 30:
+		elif player_angle >= -45 and player_angle <=45:
 			direction = "right"
+		else:
+			direction = "left"
 		animations.play("walk_"+direction)
+
 
 
 
@@ -33,17 +47,41 @@ func _on_detection_area_body_entered(body):
 
 
 func _on_detection_area_body_exited(body):
-	player = null
-	player_chase = false
+		player = null
+		player_chase = false
+
+func knockback(enemyVelocity: Vector2):
+	velocity = (player.velocity - velocity).normalized() * knockbackPower
+	move_and_slide()
+	stunned = true
+	await get_tree().create_timer(0.5).timeout
+	stunned = false
 
 func _physics_process(delta):
+	if isDead: return
 	if player_chase:
 		velocity = (player.position - position).normalized() * speed
-		move_and_slide()
+		if !stunned:
+			move_and_slide()
 	else:
 		velocity = Vector2(0,0)
 	updateAnimation()
 
 
+func _ready():
+	set_health_bar()
 
+func _on_hurt_box_area_entered(area):
 
+	if area == $hitBox: return
+	health = (health - area.get_parent().get_parent().weapon.weaponDamage)
+	set_health_bar()
+	print(healthBar.value)
+	knockback(velocity)
+	if health <= 0:  
+		$hitBox.process_mode = Node.PROCESS_MODE_DISABLED
+		isDead = true
+		healthBar.visible = false
+		animations.play("death")
+		await animations.animation_finished
+		queue_free()
--- Project Main/inventory_gui.gd
+++ Project Main/inventory_gui.gd
@@ -6,15 +6,37 @@ signal closed
 var isOpen = false
 
 @onready var inventory: Inventory = preload("res://inventory/playerInventory.tres")
-@onready var slots: Array = $NinePatchRect/GridContainer.get_children()
+@onready var slots = $NinePatchRect/GridContainer.get_children()
+@onready var ItemStackGuiClass = preload("res://itemsStackGUI.tscn")
+
+var itemInHand: ItemStackGui
 
 func _ready():
+	connectSlots()
 	inventory.updated.connect(update)
 	update()
 
+func connectSlots():
+	for i in range(slots.size()):
+		var slot = slots[i]
+		slot.index = i
+		var callable = Callable(onSlotClicked)
+		callable = callable.bind(slot)
+		slot.pressed.connect(callable)
+
 func update():
-	for i in range(min(inventory.items.size(), slots.size())):
-		slots[i].update(inventory.items[i])
+	for i in range(min(inventory.slots.size(), slots.size())):
+		var inventorySlot: InventorySlot = inventory.slots[i]
+		
+		if !inventorySlot.item: continue
+		
+		var itemStackGui: ItemStackGui = slots[i].itemStackGui
+		if !itemStackGui:
+			itemStackGui = ItemStackGuiClass.instantiate()
+			slots[i].insert(itemStackGui)
+			
+		itemStackGui.inventorySlot = inventorySlot
+		itemStackGui.update()
 
 func open():
 	visible = true
@@ -25,3 +47,34 @@ func close():
 	visible = false
 	isOpen = false
 	closed.emit()
+
+func onSlotClicked(slot):
+	if slot.isEmpty() && itemInHand:
+		insertItemInSlot(slot)
+		return
+		
+	if !itemInHand:
+		takeItemFromSlot(slot)
+
+
+func takeItemFromSlot(slot):
+	itemInHand = slot.takeItem()
+	add_child(itemInHand)
+	updateItemInHand()
+
+
+func insertItemInSlot(slot):
+	var item = itemInHand
+	
+	remove_child(itemInHand)
+	itemInHand = null
+	
+	slot.insert(item)
+
+func updateItemInHand():
+	if !itemInHand: return
+	itemInHand.global_position = get_global_mouse_position() - itemInHand.size / 2
+
+
+func _input(event):
+	updateItemInHand()
