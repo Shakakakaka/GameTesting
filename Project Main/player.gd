8e583143147316d471f3c18dfcd8e1fc0922867f
Shakakaka <shakaboom1234@gmail.com>
2024-07-09 17:41:49 -0400

    Add files via upload

--- Project Main/player.gd
+++ Project Main/player.gd
@@ -7,25 +7,66 @@ signal healthChanged
 @onready var collisionShape = $CollisionShape2D 
 @onready var effects = $Effects
 @onready var hurtbox = $hurtBox/CollisionShape2D
+@onready var weapon = $weapon
 @onready var Originx = 182
 @onready var Originy = 158
+@onready var playerDamage = 10
 
 @export var maxHealth = 10
 @onready var currentHealth = maxHealth
 @export var knockbackPower = 2000
 @export var inventory: Inventory
+var lastAnimDirection = "down"
+var isAttacking = false
+signal dead
 
 func _ready():
 	effects.play("RESET")
+	weapon.process_mode = Node.PROCESS_MODE_DISABLED
+	weapon.visible = false
+
 
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = moveDirection * speed
+	
+	if Input.is_action_just_pressed("attack"):
+			attack() 
 	Sprint()
 	noClip()
+	reloadScene()
+
+func reloadScene():
+	if Input.is_physical_key_pressed(KEY_R):
+		get_tree().reload_current_scene()
+
+func attack():
+	isAttacking = true
+	weapon.visible = true
+	weapon.process_mode = Node.PROCESS_MODE_INHERIT
+	playerDamage = weapon.weaponDamage
+	var mouseAngle
+	var mouseDirection = "up"
+	mouseAngle = rad_to_deg(get_local_mouse_position().angle())
+	if mouseAngle >= -135 and mouseAngle <= -45:
+		mouseDirection = "up"
+	elif mouseAngle <= 135 and mouseAngle >= 45:
+		mouseDirection = "down"
+	elif mouseAngle >= -45 and mouseAngle <=45:
+		mouseDirection = "right"
+	else:
+		mouseDirection = "left"
+	animations.play("attack" + mouseDirection)
+	await animations.animation_finished
+	animations.play("walk_" + mouseDirection)
+	animations.stop()
+	weapon.visible = false
+	isAttacking = false
+	weapon.process_mode = Node.PROCESS_MODE_DISABLED
 
 
 func updateAnimation():
+	if isAttacking: return
 	if velocity.is_zero_approx():
 		animations.stop()
 	else:
@@ -34,6 +75,7 @@ func updateAnimation():
 		elif velocity.x > 0: direction = "right"
 		elif velocity.y <0: direction = "up"
 		animations.play("walk_" + direction)
+		lastAnimDirection = direction
 
 func Sprint():
 	if Input.is_physical_key_pressed(KEY_SHIFT):
@@ -61,13 +103,12 @@ func _physics_process(delta):
 	updateAnimation()
 
 
-
 func hurtByEnemy(area):
 	if area.name == "hitBox":
 		currentHealth -= 1
 		if currentHealth == 0:
-			currentHealth = maxHealth
-			position = Vector2(Originx,Originy)
+			emit_signal("dead")
+			print(dead)
 		healthChanged.emit(currentHealth)
 		knockback(area.get_parent().velocity)
 		effects.play("hurtBlink")
